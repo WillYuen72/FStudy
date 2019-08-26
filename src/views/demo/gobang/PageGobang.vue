742c47a5b099e78ce6544320e3a7f55d4c345b9c
WillYuen <will.yuen@transact24.com>
2019-08-26 11:13:23 +0800

    will test

---
+++ src/views/demo/gobang/PageGobang.vue
@@ -0,0 +1,15 @@
+<template>
+    <Gobang></Gobang>
+    <!-- <p>111</p> -->
+</template>
+
+<script>
+import Gobang from '../../../components/gobang/gobang'
+
+export default {
+    name : 'demo-gobang',
+    components: {
+    [Gobang.name]: Gobang
+  },
+}
+</script>
